898278b21fe8cefda29170715898d8a7962cd4b7
Anonymous <jychuah@gmail.com>
2015-03-13 17:00:46 +0000

    Generates a blank state, tries to read (and sync) a state from Firebase. Falls back to config file.

--- data.py
+++ data.py
@@ -1,26 +1,87 @@
 # data.py
 # Interfaces with WebPersistent.com to store and retrieve controller data
 
-from WebPersistent import WebPersistent
+from firebase import firebase
+from sseclient import SSEClient
 import time
 import threading
 import json
 
 class Data:
-    state = { 'Temperature': 0, 'Humidity': 0, 'Fans': 'off', 'Lights': 'override',
-                'Channels' : [[10] * (24 * 60 / 30)] * 4}    
-    __running = False
-    __getting = False
-    __posting = False
-    __workerThread = None
-    __wp = None
+    state = { }
+    firebase = None
     
-    def __init__(self, configfile="config.txt"):
-        self.__wp = WebPersistent()
+    __FIREBASE_GET_ERROR = 0;
+    __FIREBASE_EMPTY_DATASET = 1;
+    __FIREBASE_EMPTY_INCOMPLETE_DATA = 2;
+    
+    def __init__(self):
+    
+    def begin(self):
+        start_firebase()
+    	  state = blankstate()
+        fbstate = get_initial_firebase_state()
+        if not fbstate is None:
+        		state.update(fbstate)
+        else:
+            if read_config():
+            	 # couldn't read firebase, found a config file
+            else:
+              	 # couldn't firebase, couldn't read a config file
+              	 
+              
+            
+    
+    def start_firebase(self, configfile="firebase.txt"):
         lines = [line.strip() for line in open(configfile)]
-        self.__wp.device = lines[0]
-        self.__wp.getkey = lines[1]
-        self.__wp.postkey = lines[2]
+        if len(lines) >= 3:
+	         firebaseurl = lines[0]
+   	      email = lines[1]
+      	   secret = lines[2]
+        		self.firebase = firebase.FirebaseApplication(firebaseurl, firebase.FirebaseAuthentication(secret, email, extra={'id': 1})
+            
+    def get_initial_firebase_state(self):
+		  try:
+				result = self.firebase.get('/state', None)
+				if 'error' in result:
+				    return None
+		  except IOError:
+				return None
+		  finally:
+				return result			   
+                                                         
+    def read_config(self):
+        try:
+            f = open("config.txt", "r")
+            config_state = json.load(f)
+        except IOError:
+            return False
+        finally:
+          	self.state = config_state
+            f.close()
+            return True
+            
+
+	 def blankstate():
+        state = { }
+        state['status'] = { 'hostname' : 'paludarium', 'humidity' : 0, 'temperature' : 0, 'format' : 'fahrenheit', 'lights' : 'override' }
+        state['channels'] = { x : 100 for x in range(4) }
+        state['programs'] = { x : "program " + str(x) for x in range(5)}
+        state['relays'] = { x : "off" for x in range(2) }
+        state['programdata'] = { x : new_programdata() for x in range(5) }
+        return state
+        
+
+	 def new_programdata():
+        programdata = { 'channels' : { } }
+        for x in range(4):
+        		programdata['channels'].update(new_channeldata(x))         	
+        return programdata
+        
+    
+	 def new_channeldata(channelnum):
+        return { channelnum : {x : 100 for x in range(0, 24* 60, 30)}}
+
         
     def begin(self):
         print "Data thread beginning"
