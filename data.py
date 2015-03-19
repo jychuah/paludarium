227f91a14ff31477daf1799fa65b25ad36fe3e30
jychuah <jychuah@gmail.com>
2015-03-19 17:51:00 -0500

    Added simple SSE Client callback framework

--- data.py
+++ data.py
@@ -8,6 +8,8 @@ import requests
 import time
 import threading
 import json
+from exceptions import ValueError
+import logging
 
 class Data:
     state = { }
@@ -16,32 +18,49 @@ class Data:
     firebaseurl = ""
     email = ""
     secret = ""
-
+    log = None
+    callbacks = { }
 
     def __init__(self):
-        print("init")
+        self.log = logging.getLogger("paludraium.data")
+        self.log.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        self.log.addHandler(ch)
+        self.log.debug("init Data")
 
     def begin(self):
         self.start_firebase()
         self.state = self.new_default_config()
         firebase_state = self.get_initial_firebase_state()
+        self.log.debug("Firebase state after get_initial_firebase_state: " + self.__print_pretty(firebase_state))
         if not firebase_state is None:
             # found and read firebase
-            state.update(firebase_state)
+            self.state.update(firebase_state)
+            self.log.debug("Updated local state from Firebase" + self.__print_pretty(self.state))
         else:
             # couldn't find firebase, trying to read config from file
+            self.log.debug("Could not connect to Firebase, attempting to read local config")
             config_state = self.read_config()
             if not config_state is None:
                 # found config, updating default config
+                self.log.debug("Updating local state from config file")
                 self.state.update(config_state)
+            else:
+                self.log.info("Could not read a Firebase configuration or a local configuration. Creating default configuration.")
 
         # patch firebase with any new default data
+        self.log.debug("Attempting to write complete local state to Firebase")
         if not self.overwrite_firebase_state():
-            print("Could not write firebase state")
+            self.log.info("Could not write local state to Firebase")
 
         # save config to local file
+        self.log.debug("Writing local state to local configuration file")
         if not self.write_config(self.state):
-            print("Could not write config.txt")
+            self.log.info("Could not write local state to local configuration file")
+
+    def register_callback(self, path, callback):
+        callbacks[path] = callback
 
     def overwrite_firebase_state(self):
         if self.firebase is None:
@@ -58,12 +77,13 @@ class Data:
             self.firebaseurl = lines[0]
             self.email = lines[1]
             self.secret = lines[2]
-            auth_payload = {"email": email , "uid" : "1"}
-            token = create_token(secret, auth_payload)            
-            self.firebase = Firebase(firebaseurl, auth_token=token)
+            auth_payload = {"email": self.email , "uid" : "1"}
+            token = create_token(self.secret, auth_payload)
+            self.firebase = Firebase(self.firebaseurl, auth_token=token)
                     
     def get_initial_firebase_state(self):
         if self.firebase is None:
+            self.log.debug("get_initial_firebase_state: No Firebase connection available")
             return None
         try:
             result = self.firebase.child('state').get()
@@ -72,17 +92,17 @@ class Data:
             if 'error' in result:
                 return None
             else:
-                return None
+                return result
         except HTTPError:
             return None
-                                                                                                             
+
     def read_config(self):
         try:
             f = open("config.txt", "r")
             config_state = json.load(f)
             f.close()
             return config_state
-        except IOError:
+        except (IOError, ValueError):
             return None
         return None
 
@@ -97,13 +117,13 @@ class Data:
 
     def new_default_config(self):
         state = { }
-        state['version'] = "0.1"
-        state['status'] = { 'hostname' : 'paludarium', 'humidity' : 0, 'temperature' : 0, 'format' : 'fahrenheit', 'lights' : 'override', 'currentprogram' : -1 }
-        state['channels'] = { x : 100 for x in range(4) }
-        state['channeldefaults'] = { x : 100 for x in range(4) }
+        state['status'] = { 'version' : '0.1', 'hostname' : 'paludarium', 'humidity' : 0, 'temperature' : 0  }
+        state['status']['channels'] = { x : 100 for x in range(4) }
+        state['control'] = { 'format' : 'fahrenheit', 'lights' : 'override', 'current_program' : -1 }
+        state['control']['relays'] = { x : "off" for x in range(2) }
+        state['control']['override_values'] = { x : 100 for x in range(4) }
         state['programs'] = { x : "program " + str(x) for x in range(5)}
-        state['relays'] = { x : "off" for x in range(2) }
-        state['programdata'] = { x : self.new_programdata() for x in range(5) }
+        state['program_data'] = { x : self.new_programdata() for x in range(5) }
         return state
 
     def new_programdata(self):
@@ -124,24 +144,35 @@ class Data:
         except HTTPError:
             return False
 
-    def connect_sse(self):
+    def connect_sse_state(self):
         if len(self.firebaseurl) > 0:
-            self.sse = ClosableSSEClient(self.firebaseurl)
+            # connect to firebase SSE
+            self.sse = ClosableSSEClient(self.firebaseurl.rstrip("/") + "/state.json")
+            self.log.info("Connected SSE Client to Firebase")
             for msg in self.sse:
-                if not msg is None or not msg.data is None:
+                self.log.debug("SSE Event received")
+                if msg is None or msg.data is None:
+                    self.log.debug("SSE Event was empty")
                     continue
-                msg_data = json.loads(msg.data)
-                path = msg_data['path']
-                data = msg_data['data']
+                self.log.debug("Got valid SSE Event: " + msg.dump())
+                try:
+                    msg_data = json.loads(msg.data)
+                    path = msg_data['path']
+                    data = msg_data['data']
+                    self.process_sse_event(path, data)
+                except ValueError:
+                    self.log.debug("Could not decode SSE Event")
+                except TypeError:
+                    self.log.debug("Could not decode SSE Event, maybe a keep-alive event")
 
     def process_sse_event(self, path, data):
-        print("Processing SSE event")
-        if path.find("/state") > -1:
-            path = path[len('/state'):]
-        node = self.state
-        for child in path.split('/'):
-            node = node[child]
-        node.update(data)
+        if not path == "/":
+            for key in callbacks.keys():
+                if key in path:
+                    callbacks[key](path, data)
+
+    def __print_pretty(self, data):
+        return json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
 
 
 class ClosableSSEClient(SSEClient):
@@ -167,3 +198,7 @@ class ClosableSSEClient(SSEClient):
         # requests library to make this easier
         self.resp.raw._fp.fp._sock.shutdown(socket.SHUT_RDWR)
         self.resp.raw._fp.fp._sock.close()
+
+d = Data()
+d.begin()
+d.connect_sse_state()
