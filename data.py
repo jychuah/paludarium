a5f6327deb08f576db334c05c3542f40b0852343
jchuah <jchuah@austinisd.org>
2015-04-02 15:55:53 -0500

    Pared down Firebase initialization Now receives SSE events and updates controller state GTK Window now shows controller state

--- data.py
+++ data.py
@@ -20,9 +20,12 @@ class Data:
     secret = ""
     log = None
     callbacks = { }
+    ref = None
+    controller_id = ""
+    connected = False
 
     def __init__(self):
-        self.log = logging.getLogger("paludraium.data")
+        self.log = logging.getLogger("paludarium.data")
         self.log.setLevel(logging.DEBUG)
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
@@ -31,33 +34,6 @@ class Data:
 
     def begin(self):
         self.start_firebase()
-        self.state = self.new_default_config()
-        firebase_state = self.get_initial_firebase_state()
-        self.log.debug("Firebase state after get_initial_firebase_state: " + self.__print_pretty(firebase_state))
-        if not firebase_state is None:
-            # found and read firebase
-            self.state.update(firebase_state)
-            self.log.debug("Updated local state from Firebase" + self.__print_pretty(self.state))
-        else:
-            # couldn't find firebase, trying to read config from file
-            self.log.debug("Could not connect to Firebase, attempting to read local config")
-            config_state = self.read_config()
-            if not config_state is None:
-                # found config, updating default config
-                self.log.debug("Updating local state from config file")
-                self.state.update(config_state)
-            else:
-                self.log.info("Could not read a Firebase configuration or a local configuration. Creating default configuration.")
-
-        # patch firebase with any new default data
-        self.log.debug("Attempting to write complete local state to Firebase")
-        if not self.overwrite_firebase_state():
-            self.log.info("Could not write local state to Firebase")
-
-        # save config to local file
-        self.log.debug("Writing local state to local configuration file")
-        if not self.write_config(self.state):
-            self.log.info("Could not write local state to local configuration file")
 
     def register_callback(self, path, callback):
         callbacks[path] = callback
@@ -75,26 +51,18 @@ class Data:
         lines = [line.strip() for line in open(configfile)]
         if len(lines) >= 3:
             self.firebaseurl = lines[0]
-            self.email = lines[1]
-            self.secret = lines[2]
-            auth_payload = {"email": self.email , "uid" : "1"}
+            self.secret = lines[1]
+            self.controller_id = lines[2]
+            uid = 'controller:' + self.controller_id
+            auth_payload = {"controller_id": self.controller_id, "uid" : uid }
             token = create_token(self.secret, auth_payload)
             self.firebase = Firebase(self.firebaseurl, auth_token=token)
-                    
-    def get_initial_firebase_state(self):
-        if self.firebase is None:
-            self.log.debug("get_initial_firebase_state: No Firebase connection available")
-            return None
-        try:
-            result = self.firebase.child('state').get()
-            if result is None:
-                return None
-            if 'error' in result:
-                return None
-            else:
-                return result
-        except HTTPError:
-            return None
+            self.ref = self.firebase.child("controllers").child(self.controller_id)
+        else:
+            self.log.critical("firebase file was incomplete")
+
+    def write_status(self, path, value):
+        self.ref.child(path).set(value)
 
     def read_config(self):
         try:
@@ -115,61 +83,46 @@ class Data:
         except IOError:
             return False
 
-    def new_default_config(self):
-        state = { }
-        state['status'] = { 'version' : '0.1', 'hostname' : 'paludarium', 'humidity' : 0, 'temperature' : 0  }
-        state['status']['channels'] = { x : 100 for x in range(4) }
-        state['control'] = { 'format' : 'fahrenheit', 'lights' : 'override', 'current_program' : -1 }
-        state['control']['relays'] = { x : "off" for x in range(2) }
-        state['control']['override_values'] = { x : 100 for x in range(4) }
-        state['programs'] = { x : "program " + str(x) for x in range(5)}
-        state['program_data'] = { x : self.new_programdata() for x in range(5) }
-        return state
-
-    def new_programdata(self):
-        programdata = { 'channels' : { } }
-        for x in range(4):
-            programdata['channels'].update(self.new_channeldata(x))          
-        return programdata
-
-    def new_channeldata(self, channelnum):
-        return { channelnum : {x : 100 for x in range(0, 24* 60, 30)}}
-
-    def log_data(self, path, value):
-        if self.firebase is None:
-            return False
-        try:
-            self.firebase.child('data').child(path).put({ 'timestamp' : { '.sv' : 'timestamp'}, 'value' : value})
-            return True
-        except HTTPError:
-            return False
-
-    def connect_sse_state(self):
+    def connect_sse_state(self, update_receiver):
         if len(self.firebaseurl) > 0:
             # connect to firebase SSE
-            self.sse = ClosableSSEClient(self.firebaseurl.rstrip("/") + "/state.json")
+            self.sse = ClosableSSEClient(self.firebaseurl.rstrip("/") + "/controllers/" + self.controller_id + ".json")
             self.log.info("Connected SSE Client to Firebase")
             for msg in self.sse:
                 self.log.debug("SSE Event received")
                 if msg is None or msg.data is None:
                     self.log.debug("SSE Event was empty")
                     continue
-                self.log.debug("Got valid SSE Event: " + msg.dump())
                 try:
                     msg_data = json.loads(msg.data)
                     path = msg_data['path']
                     data = msg_data['data']
                     self.process_sse_event(path, data)
+                    update_receiver.update()
                 except ValueError:
                     self.log.debug("Could not decode SSE Event")
                 except TypeError:
                     self.log.debug("Could not decode SSE Event, maybe a keep-alive event")
+                time.sleep(0.2);
+            self.log.info("SSE Client Disconnected")
 
     def process_sse_event(self, path, data):
-        if not path == "/":
-            for key in callbacks.keys():
-                if key in path:
-                    callbacks[key](path, data)
+        if path == "/":
+            self.state = data
+            self.connected = True
+        else:
+            paths = path.split("/")
+            last_key = paths[len(paths) - 1]
+            paths = paths[1:len(paths) - 1]
+            node = self.state
+            for child in paths:
+                if child.isdigit():
+                    child = int(child)
+                node = node[child]
+            if last_key.isdigit():
+                last_key = int(last_key)
+            node[last_key] = data
+        self.log.debug("data: " + self.__print_pretty(self.state))
 
     def __print_pretty(self, data):
         return json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
@@ -198,7 +151,3 @@ class ClosableSSEClient(SSEClient):
         # requests library to make this easier
         self.resp.raw._fp.fp._sock.shutdown(socket.SHUT_RDWR)
         self.resp.raw._fp.fp._sock.close()
-
-d = Data()
-d.begin()
-d.connect_sse_state()
--- paludarium.py
+++ paludarium.py
@@ -1,36 +1,97 @@
 #!/usr/bin/python
 
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib, GObject
+from requests import ConnectionError
 import threading
 import time
 from gi.repository import GObject
+from data import Data
+from threading import Thread
+from time import sleep
 
 win = None
 updateNum = 1
+data = Data()
+data.begin()
 
 class PaludariumWindow(Gtk.Window):
-	def __init__(self):
-		Gtk.Window.__init__(self, title="Monsoon Birder")
-		self.button = Gtk.Button(label="Click Here")
-		self.button.connect("clicked", self.on_button_clicked)
-		self.add(self.button)
+    updateNum = 0
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Paludarium")
+        self.connect("delete-event", Gtk.main_quit)
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=2)
+        self.add(self.box)
+        self.humidity = Gtk.Label("Humidity")
+        self.box.pack_start(self.humidity, True, True, 0)
+        self.temperature = Gtk.Label("temperature")
+        self.box.pack_start(self.temperature, True, True, 0)
+        self.current_program = Gtk.Label("current_program")
+        self.box.pack_start(self.current_program, True, True, 0)
+        self.lights_control = Gtk.Label("lights control")
+        self.box.pack_start(self.lights_control, True, True, 0)
+        self.lights_0 = Gtk.Label("lights 0")
+        self.box.pack_start(self.lights_0, True, True, 0)
+        self.lights_1 = Gtk.Label("lights 1")
+        self.box.pack_start(self.lights_1, True, True, 0)
+        self.lights_2 = Gtk.Label("lights 2")
+        self.box.pack_start(self.lights_2, True, True, 0)
+        self.lights_3 = Gtk.Label("lights 3")
+        self.box.pack_start(self.lights_3, True, True, 0)
+        self.relays_control = Gtk.Label("relays control")
+        self.box.pack_start(self.relays_control, True, True, 0)
+        self.relays_0 = Gtk.Label("relay 0")
+        self.box.pack_start(self.relays_0, True, True, 0)
+        self.relays_1 = Gtk.Label("relay 1")
+        self.box.pack_start(self.relays_1, True, True, 0)
 
-	def on_button_clicked(self, widget):
-		print("Hello World")
+    def map_status(self):
+        self.set_title(data.state["name"])
+        self.current_program.set_text("Current program: " + data.state["state"]["current_program"])
+        self.temperature.set_text("Temperature: " + str(data.state["state"]["temperature"]))
+        self.humidity.set_text("Humidity: " + str(data.state["state"]["humidity"]))
+        self.lights_control.set_text("Lights: " + str(data.state["state"]["lights"]["control"]))
+        self.lights_0.set_text(data.state["state"]["lights"]["names"][0] + ": " + str(data.state["state"]["lights"]["values"][0]))
+        self.lights_1.set_text(data.state["state"]["lights"]["names"][1] + ": " + str(data.state["state"]["lights"]["values"][1]))
+        self.lights_2.set_text(data.state["state"]["lights"]["names"][2] + ": " +  str(data.state["state"]["lights"]["values"][2]))
+        self.lights_3.set_text(data.state["state"]["lights"]["names"][3] + ": " +  str(data.state["state"]["lights"]["values"][3]))
+        self.relays_control.set_text("Relays: " + str(data.state["state"]["relays"]["control"]))
+        self.relays_0.set_text(data.state["state"]["relays"]["names"][0] + ": " +  str(data.state["state"]["relays"]["values"][0]))
+        self.relays_1.set_text(data.state["state"]["relays"]["names"][1] + ": " +  str(data.state["state"]["relays"]["values"][1]))
 
-
+    def update(self):
+        GObject.timeout_add(1, self.map_status())
 
 def timeout():
-	global updateNum
-	global win
-	updateNum = updateNum + 1
-	win.button.set_label(str(updateNum))
-	GObject.timeout_add(2000, timeout)
+    global data
+    if data.connected:
+        try:
+            data.ref.child("state/temperature").set(int(data.state["state"]["temperature"]) + 1)
+        except ConnectionError:
+            data.log.info("Could not write to Firebase")
 
-			
+    GObject.timeout_add(2000, timeout)
 
-win = PaludariumWindow()
-win.connect("delete-event", Gtk.main_quit)
-win.show_all()
-GObject.timeout_add(2000, timeout)
-Gtk.main()
+
+def app_main():
+    global win
+    win = PaludariumWindow()
+
+    def example_target():
+        data.connect_sse_state(win)
+
+    win.show_all()
+    GObject.timeout_add(2000, timeout)
+
+    thread = threading.Thread(target=example_target)
+    thread.daemon = True
+    thread.start()
+
+
+
+
+if __name__ == "__main__":
+    # Calling GObject.threads_init() is not needed for PyGObject 3.10.2+
+    GObject.threads_init()
+
+    app_main()
+    Gtk.main()
